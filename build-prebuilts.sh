5b3ac3e63327ddc834b216fb90ffbb1308a29428
Lifang Xia <xlf194833@alibaba-inc.com>
2020-07-24 17:31:07 +0800

    add lib and bin for riscv64 building
    Change-Id: Iabe4fa3aa81cb334f92de1fcce3b1a49c456a051
    Signed-off-by: CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>

--- build-prebuilts.sh
+++ build-prebuilts.sh
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 export LLVM_BUILD_HOST_TOOLS=true
-export LLVM_PREBUILTS_VERSION=clang-r353983b
-export LLVM_RELEASE_VERSION=9.0.2
+export LLVM_PREBUILTS_VERSION=clang-dev
+export LLVM_RELEASE_VERSION=11.0.3
 
 # FIXME: Workaround to build bionic versioner in the aosp/clang-tools branch.
 export FORCE_BUILD_LLVM_COMPONENTS=true
